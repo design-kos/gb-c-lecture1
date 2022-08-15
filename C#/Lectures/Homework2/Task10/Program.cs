cce8aebbeb28f3fcb2705212d24e0efd75d7175e
«AKoshelya» <design-kos@ya.ru>
2022-08-15 11:02:43 +0500

    Add Task10

--- C#/Lectures/Homework2/Task10/Program.cs
+++ C#/Lectures/Homework2/Task10/Program.cs
@@ -1,33 +1,40 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 using System;
- 
+
 namespace Homeworks
 {
     class Program
     {
+        static int FindSecondDigitOfNumber(int N)
+        {
+            int secondDigitOfNumber = N / 10 % 10;
+            return secondDigitOfNumber;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите трехзначное число: ");
-            int N = Convert.ToInt32(Console.ReadLine());   
 
-            while (N > 0) 
+            Console.WriteLine("Введите трехзначное число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            while (N > 0)
             {
-                if (N < 99) 
+                if (N < 99)
                 {
                     Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число: ");
-                    N = Convert.ToInt32(Console.ReadLine());   
+                    N = Convert.ToInt32(Console.ReadLine());
                 }
                 else if (N > 999)
                 {
                     Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число: ");
-                    N = Convert.ToInt32(Console.ReadLine());   
+                    N = Convert.ToInt32(Console.ReadLine());
                 }
-                else 
+                else
                 {
-                    int secondDigit = N / 10 % 10;
-                    Console.WriteLine($"Вторая цифры введенного числа - {secondDigit}");  
-                    break;                 
+                    int secondDigit = FindSecondDigitOfNumber(N);
+                    Console.WriteLine($"Вторая цифры введенного числа - {secondDigit}");
+                    break;
                 }
             }
         }
