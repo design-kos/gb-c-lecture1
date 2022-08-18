a6b272ee37fb45c117a263867a22b331c25f5599
«AKoshelya» <design-kos@ya.ru>
2022-08-18 11:06:25 +0500

    Add Task23

---
+++ C#/Lectures/Homeworks3/Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число, до которого будет создана таблица кубов: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Число {i} в кубе равно {i*i*i}"); 
+            }
+        }
+    }
+}
