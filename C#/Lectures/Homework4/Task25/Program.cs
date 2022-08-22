ee7a21b930a74b52da777c918d2648c083e5f503
«AKoshelya» <design-kos@ya.ru>
2022-08-22 13:45:25 +0500

    Add Task25

---
+++ C#/Lectures/Homework4/Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static int Exponentiation(int A, int B)
+        {
+            int result = 1;
+            for (int i = 1; i <= B; i++)
+            {
+                result = result * A;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень, в которую нужно возвести число: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int exponentiation = Exponentiation(A, B);
+            Console.WriteLine($"{A}^{B} = " + exponentiation);
+        }
+    }
+}
