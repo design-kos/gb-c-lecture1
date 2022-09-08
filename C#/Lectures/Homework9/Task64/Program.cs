0f66463eba3123ab3af4ded948327b4aa588b01a
«AKoshelya» <design-kos@ya.ru>
2022-09-08 10:34:54 +0500

    Add Task64

---
+++ C#/Lectures/Homework9/Task64/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+
+
+using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (m > n)
+            {
+                for (int i = m; i >= n; i--)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            else
+            {
+                for (int i = n; i >= m; i--)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+        }
+    }
+}
